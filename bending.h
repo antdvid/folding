76a94011c63f8d2d66b12da2c02c4556026c35e8
ytbwd <tengbo.yang@stonybrook.edu>
2017-01-26 14:24:36 -0500

    add method to bending

--- bending.h
+++ bending.h
@@ -4,12 +4,20 @@
 class BendingForce: public SpringSolver::ExtForceInterface
 {
     INTERFACE* intfc;
+    // bend stiffness
+    double bends; 
+    // bend damping
+    double bendd; 
     void calculateBendingForce3d2003(POINT* p1, TRI* t1, TRI* t2);
     void calculateBendingForce3d2006(POINT* p1, TRI* t1, TRI* t2);
     void clear_surf_point_force(SURFACE*);
 public: 
     double* getExternalForce(SpringVertex* sv);
     void computeExternalForce();
-    BendingForce(INTERFACE* _intfc) : intfc(_intfc) {}
+    void getParaFromFile(const char*); 
+    double& getBendStiff() { return bends; }
+    double& getBendDamp() { return bendd; }
+    BendingForce(INTERFACE* _intfc, double s = 0.01, double d = 0.0) : 
+		intfc(_intfc) { bends = s; bendd = d; }
 };
 
